20ce5c869426629dbda2037e90416d5fc8f79295
wellenvogel <andreas@wellenvogel.de>
2026-01-14 19:40:42 +0100

    dump yaml

--- yt.py
+++ yt.py
@@ -1,10 +1,23 @@
 #! /usr/bin/env python3
 import sys
 import yaml
+import pprint
 from yaml import Loader
-def check(inf):
+def check(inf,dump=False):
     with open(inf,"r") as i:
         doc=yaml.load(i,Loader=Loader)
-    
+    if dump:
+        pprint.pp(doc,indent=2)
 
-check(sys.argv[1])        
+dump=False    
+if len(sys.argv) > 2:
+    if sys.argv[1] == '-d':
+        dump=True
+    else:
+        print(f"invalid otion {sys.argv[1]}",file=sys.stderr)
+        sys.exit(1)
+    name=sys.argv[2]
+else:
+    name=sys.argv[1]    
+
+check(name,dump)        
